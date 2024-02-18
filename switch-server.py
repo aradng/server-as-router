15a26873dd823668a0967ca21e69bfe904a3b862
aradng <aradarang@gmail.com>
2024-02-18 21:52:51 +0330

    switch-server fix script runs

--- switch-server.py
+++ switch-server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 import os
 
 app = FastAPI()
@@ -8,15 +9,17 @@ app = FastAPI()
 async def change_state(state: str):
     if state == "on":
         # turn vpn on
-        os.system(
-            "tailscale up --reset --advertise-routes=192.168.1.0/24 --exit-node-allow-lan-access --exit-node=100.64.0.2"
-        )
-        return {"state": state}
+        cmd = "sudo tailscale up --reset --accept-routes --advertise-routes=192.168.1.0/24 --exit-node-allow-lan-access --exit-node=100.64.0.2"
+        os.system(cmd)
+        return cmd
     elif state == "off":
         # turn vpn off
-        os.system(
-            "tailscale up --reset --advertise-routes=192.168.1.0/24 --advertise-exit-node"
-        )
-        return {"state": state}
+        cmd = "sudo tailscale up --reset --accept-routes --advertise-routes=192.168.1.0/24 --advertise-exit-node"
+        os.system(cmd)
+        return cmd
     else:
-        return {"state": "error"}
+        return "error"
+
+
+if __name__ == "__main__":
+    uvicorn.run("switch-server:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
