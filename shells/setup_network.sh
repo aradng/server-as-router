1f971a59165413ed1862b45e8d2307a1f5767f32
aradng <aradarang@gmail.com>
2024-07-18 17:22:48 +0330

    make setup_routes.sh more customizable

--- shells/setup_network.sh
+++ shells/setup_network.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+# machine ip and iface in secondary subnet
+GATEWAY=31.14.122.33
+SUBNET=31.14.122.32/27
+IP=$(ip route | grep $SUBNET | awk '{print $9}')
+IFACE=$(ip route | grep $SUBNET | awk '{print $3}')
 
 # send to correct iface and gateway
-ip route add default via 31.14.122.33 table 200
-
-# machine ip in secondary subnet
-IP=$(ip route | grep 31.14.122.32/27 | awk '{print $9}')
+ip route add default via $GATEWAY table 200
 
 # route packets originating from this machine with secondary subnet
 ip rule add from $IP table 200 priority 100
 
 # iptables RETURN target before docker DNAT for public facing iface
-iptables -t nat -A PREROUTING ! -s 31.14.122.32/27 -i eth1 -j RETURN
+iptables -t nat -I PREROUTING ! -s $SUBNET -i $IFACE -j RETURN
 
 
 
